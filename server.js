a0a484e57c3ad2d136ebad8cf22cd8b498270c82
ShazibKhattal99 <shazibkhattal99@gmail.com>
2025-02-12 16:13:34 +0530

    cors added

--- server.js
+++ server.js
@@ -2,10 +2,12 @@ import express from "express";
 import dotenv from 'dotenv';
 import connectDB from "./config/db.js";
 import otpRoutes from "./routes/whatsapp/otp.routes.js";
+import cors from 'cors';
 //import campaignRoutes from "./routes/whatsapp/campaign.routes.js";
 
 dotenv.config();
 const app = express();
+app.use(cors());
 app.use(express.json());
 connectDB();
 
